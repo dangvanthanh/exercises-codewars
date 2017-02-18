6dcdeee331aa582bab0a069a942092f6243681b9
Dang Van Thanh <dangvanthanh@dangthanh.org>
2017-02-18 16:37:44 +0700

    To leet speak

---
+++ to-leet-speak/toLeetSpeak.js
@@ -0,0 +1,34 @@
+function toLeetSpeak (str) {
+  const leetSpeak = {
+    A: '@',
+    B: '8',
+    C: '(',
+    D: 'D',
+    E: '3',
+    F: 'F',
+    G: '6',
+    H: '#',
+    I: '!',
+    J: 'J',
+    K: 'K',
+    L: '1',
+    M: 'M',
+    N: 'N',
+    O: '0',
+    P: 'P',
+    Q: 'Q',
+    R: 'R',
+    S: '$',
+    T: '7',
+    U: 'U',
+    V: 'V',
+    W: 'W',
+    X: 'X',
+    Y: 'Y',
+    Z: '2'
+  }
+
+  return str.split('').map(s => leetSpeak[s] || s).join('')
+}
+
+module.exports = toLeetSpeak
